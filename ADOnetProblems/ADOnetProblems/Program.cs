fadc8b7198a494899a36a8cb821813b518552517
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-23 14:06:55 +0530

    [Abhishek]Update Data

---
+++ ADOnetProblems/ADOnetProblems/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+
+            EmployeeRepo repo = new EmployeeRepo();
+            try
+            {
+                Console.WriteLine("Choose option or press 0 for exit\n1:Retrieve Data\n2:Add Data\n3:Update Basic_Salary");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        repo.GetAllEmployees();
+                        break;
+                    case 2:
+                        EmployeeModel model = new EmployeeModel
+                        {
+                            Name = "Pant",
+                            Startdate = DateTime.Now,
+                            Gender = 'M',
+                            Phone = 912423,
+                            Department = "DC",
+                            Address = "Delhi",
+                            Basic_Pay = 30000.00,
+                            Deductions = 1000.00,
+                            Taxable_Pay = 29000.00,
+                            Income_Tax = 1000.00,
+                            Net_Pay = 28000,
+                        };
+                        repo.AddEmployee(model);
+                        repo.GetAllEmployees();
+                        break;
+                    case 3:
+                        EmployeeModel model1 = new EmployeeModel();
+                        Console.WriteLine("Enter id of employee whose data you want to update");
+                        model1.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter name");
+                        model1.Name = Console.ReadLine();
+                        Console.WriteLine("Enter new BasicPay");
+                        model1.Basic_Pay = Convert.ToDouble(Console.ReadLine());
+                        repo.UpdateEmployee(model1);
+                        repo.GetAllEmployees();
+                        break;
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
