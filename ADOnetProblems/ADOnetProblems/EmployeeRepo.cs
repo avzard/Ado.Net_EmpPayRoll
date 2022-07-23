8bffb080790fc2689982349c82d2d660374a0ca3
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-23 13:13:28 +0530

    [Abhishek]Add Employee

---
+++ ADOnetProblems/ADOnetProblems/EmployeeRepo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollSQL
+{
+    public class EmployeeRepo
+    {
+        string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=Payroll_Service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = null;
+        public void GetAllEmployees()
+        {
+            try
+            {
+                using (connection = new SqlConnection(connectionString))
+                {
+                    EmployeeModel model = new EmployeeModel();
+                    string query = "select * from employee_payroll";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            model.Id = Convert.ToInt32(reader["Id"] == DBNull.Value ? default : reader["Id"]);
+                            model.Name = Convert.ToString(reader["Name"] == DBNull.Value ? default : reader["Name"]);
+                            model.Basic_Pay = Convert.ToDouble(reader["Basic_Pay"] == DBNull.Value ? default : reader["Basic_Pay"]);
+                            model.Startdate = (DateTime)(reader["Startdate"] == DBNull.Value ? default : reader["Startdate"]);
+                            model.Gender = Convert.ToChar(reader["Gender"] == DBNull.Value ? default : reader["Gender"]);
+                            model.Phone = Convert.ToInt64(reader["Phone"] == DBNull.Value ? default : reader["Phone"]);
+                            model.Address = Convert.ToString(reader["Address"] == DBNull.Value ? default : reader["Address"]);
+                            model.Department = Convert.ToString(reader["Department"] == DBNull.Value ? default : reader["Department"]);
+                            model.Deductions = Convert.ToDouble(reader["Deductions"] == DBNull.Value ? default : reader["Deductions"]);
+                            model.Taxable_Pay = Convert.ToDouble(reader["Taxable_Pay"] == DBNull.Value ? default : reader["Taxable_Pay"]);
+                            model.Income_Tax = Convert.ToDouble(reader["Income_Tax"] == DBNull.Value ? default : reader["Income_Tax"]);
+                            model.Net_Pay = Convert.ToDouble(reader["Net_Pay"] == DBNull.Value ? default : reader["Net_Pay"]);
+                            Console.WriteLine($"{model.Id}, {model.Name}, {model.Basic_Pay}, {model.Startdate}, {model.Gender}, {model.Phone}, {model.Address}, {model.Department}, {model.Deductions}, {model.Taxable_Pay}, {model.Income_Tax}, {model.Net_Pay} \n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //finally
+            //{
+            //    connection.Close(); Require if we don't use using above 
+            //}
+        }
+        public void AddEmployee(EmployeeModel obj)
+        {
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("spAddEmployees", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Name", obj.Name);
+                command.Parameters.AddWithValue("@Gender", obj.Gender);
+                command.Parameters.AddWithValue("@Startdate", obj.Startdate);
+                command.Parameters.AddWithValue("@Phone", obj.Phone);
+                command.Parameters.AddWithValue("@Department", obj.Department);
+                command.Parameters.AddWithValue("@Address", obj.Address);
+                command.Parameters.AddWithValue("@Basic_Pay", obj.Basic_Pay);
+                command.Parameters.AddWithValue("@Deductions", obj.Deductions);
+                command.Parameters.AddWithValue("@Taxable_Pay", obj.Taxable_Pay);
+                command.Parameters.AddWithValue("@Income_Tax", obj.Income_Tax);
+                command.Parameters.AddWithValue("@Net_Pay", obj.Net_Pay);
+                connection.Open();
+                var result = command.ExecuteNonQuery();
+                if (result != 0)
+                {
+                    Console.WriteLine("Employee details added successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to add employee details");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
